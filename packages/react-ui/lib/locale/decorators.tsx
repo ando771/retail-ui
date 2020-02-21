286c806ce3d8d6fbd5c108df1ddb198548739b7f
v_sorokin <v_sorokin@kontur>
2020-02-21 12:10:31 +0500

    chore: try

--- packages/react-ui/lib/locale/decorators.tsx
+++ packages/react-ui/lib/locale/decorators.tsx
@@ -1,36 +1,25 @@
 import React from 'react';
 
 import { defaultLangCode } from './constants';
-import { LocaleContext, LocaleContextProps } from './LocaleContext';
+import { LocaleContext } from './LocaleContext';
 import { LocaleHelper } from './LocaleHelper';
 import { LangCodes, LocaleControls } from './types';
 
 export function locale<C>(controlName: keyof LocaleControls, localeHelper: LocaleHelper<C>) {
   return <T extends new (...args: any[]) => React.Component>(constructor: T) => {
     const LocaleDecorator = class extends constructor {
-      public localeContext?: LocaleContextProps;
+      public static contextType = LocaleContext;
+      public context!: React.ContextType<typeof LocaleContext>;
       public controlName: keyof LocaleControls = controlName;
       public localeHelper: LocaleHelper<C> = localeHelper;
 
-
-      public render() {
-        return (
-          <LocaleContext.Consumer>
-            {localeContext => {
-              this.localeContext = localeContext;
-              return super.render();
-            }}
-          </LocaleContext.Consumer>
-        );
-      }
-
       public get locale(): C {
-        if (this.localeContext === undefined) {
+        if (this.context === undefined) {
           return {} as C;
         }
 
-        const localeFromContext = this.localeContext.locale?.[this.controlName];
-        return Object.assign({}, this.localeHelper.get(this.localeContext.langCode), localeFromContext);
+        const localeFromContext = this.context.locale?.[this.controlName];
+        return Object.assign({}, this.localeHelper.get(this.context.langCode), localeFromContext);
       }
 
       public set locale(l: C) {
@@ -38,7 +27,7 @@ export function locale<C>(controlName: keyof LocaleControls, localeHelper: Local
       }
 
       public get langCode(): LangCodes {
-        return this.localeContext?.langCode ?? defaultLangCode;
+        return this.context.langCode ?? defaultLangCode;
       }
     };
     Object.defineProperty(LocaleDecorator, 'name', { value: constructor.name });
